857c0ef414dc5e8a4588e2d9d793d19cb7ddf8c7
youssef bouryal <youssefbouryal02@gmail.com>
2025-06-06 11:08:31 +0100

    Create elegant and luxurious landing page design

--- components/ui/smooth-scroll.tsx
+++ components/ui/smooth-scroll.tsx
@@ -1,32 +1,38 @@
 'use client';
 
 import { useEffect } from 'react';
-import Lenis from 'lenis';
 
 export function SmoothScroll() {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
-    });
+    // Import Lenis only on client side
+    const initSmoothScroll = async () => {
+      const Lenis = (await import('lenis')).default;
+      
+      const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        direction: 'vertical',
+        gestureDirection: 'vertical',
+        smooth: true,
+        mouseMultiplier: 1,
+        smoothTouch: false,
+        touchMultiplier: 2,
+        infinite: false,
+      });
 
-    function raf(time: number) {
-      lenis.raf(time);
+      function raf(time: number) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+
       requestAnimationFrame(raf);
-    }
 
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
+      return () => {
+        lenis.destroy();
+      };
     };
+
+    initSmoothScroll();
   }, []);
 
   return null;
